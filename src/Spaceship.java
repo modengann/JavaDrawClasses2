abc69b1e5bc4d30091f71239cce48840eda340f5
modengann <99417989+modengann@users.noreply.github.com>
2022-11-27 18:54:52 -0500

    Update Spaceship.java

---
+++ src/Spaceship.java
@@ -0,0 +1,20 @@
+/** Video 3: Actions - https://www.youtube.com/watch?v=_27wR7-JzDc
+ *  
+ *  Task:
+ *  -Add move method to Spaceship Class
+ *      *should move to edge of the screen and bounce
+ *  -Add getHealth method to Spaceship Class
+ *      *returns current health of Spaceship
+ *  -Add setHealth method to Spaceship Class
+ *      *sets health of Spaceship
+ * 
+ */
+import javadraw.*;
+import java.util.*;
+
+
+public class Spaceship {
+
+
+
+}
