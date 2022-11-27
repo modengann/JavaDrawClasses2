743c989b29b6601a0273bf5e2a33dcbda4118a44
modengann <99417989+modengann@users.noreply.github.com>
2022-11-27 17:29:51 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,23 @@
+/** Video 3: Actions - 
+ *  
+ *  Task:
+ *  -Add move method to Spaceship Class
+ *      *should move to edge of the screen and return
+ *  -Add getHealth method to Spaceship Class
+ *      *returns current health of Spaceship
+ *  -Add setHealth method to Spaceship Class
+ *      *sets health of Spaceship
+ * 
+ */
+
+import javadraw.*;
+import java.util.*;
+
+public class App extends Window{
+    public static void main(String[] args) throws Exception {
+        Window.open();
+    }
+    public void start(){
+
+    }
+}
